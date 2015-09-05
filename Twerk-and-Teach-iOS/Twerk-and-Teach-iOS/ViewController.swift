79f4df5361f25e0e82707ed41f9fa5dac850a8ee
ctong1234 <calvinytong@gmail.com>
2015-09-05 04:32:26 -0400

    tested beacon code

--- Twerk-and-Teach-iOS/Twerk-and-Teach-iOS/ViewController.swift
+++ Twerk-and-Teach-iOS/Twerk-and-Teach-iOS/ViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, PaerUIDelegate {
 
+    @IBOutlet weak var GoButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +20,38 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func Range(sender: AnyObject)
+    {
+        var paer = PaerManager(dictToSend: ["stuff": "things"])
+        paer.delegate = self
+        paer.start()
+    }
+    
+    func paerStarted() {
+        println("UI Received: Paer Started")
+    }
+    
+    func paerSavedContact(name: String) {
+        
+    }
+    
+    func paerFollowedTwitter(user: [String : AnyObject]) {
+        
+    }
+    
+    func paerFollowedInstagram(user: [String : AnyObject]) {
+        
+    }
+    
+    func paerCompleted() {
+        
+    }
+    
+    func paerTimedOut() {
+        
     }
 
     
