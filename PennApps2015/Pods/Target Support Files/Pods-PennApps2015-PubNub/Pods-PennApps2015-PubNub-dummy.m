4b3973e71a03ff1445becf3c09db51e560bd3bc5
ctong1234 <calvinytong@gmail.com>
2015-09-06 04:27:21 -0400

    deleted old project

--- PennApps2015/Pods/Target Support Files/Pods-PennApps2015-PubNub/Pods-PennApps2015-PubNub-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PennApps2015_PubNub : NSObject
-@end
-@implementation PodsDummy_Pods_PennApps2015_PubNub
-@end
--- PennApps2015/Pods/Target Support Files/Pods-PennApps2015/Pods-PennApps2015-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_PennApps2015VersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_PennApps2015VersionString[];
-
